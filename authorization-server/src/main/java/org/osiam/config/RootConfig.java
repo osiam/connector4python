0e35fbbfc3d50b7600d38f2c1c9c6f6c8da6b7ce
Philipp Eder <p.eder@tarent.de>
2013-02-26 17:33:46 +0100

    added gitignore and general spring 3.1 quickstart project
    added .gitignore and generated an unmodified spring 3.1 quick start project

---
+++ authorization-server/src/main/java/org/osiam/config/RootConfig.java
@@ -0,0 +1,18 @@
+package org.osiam.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+@ComponentScan(basePackages = { "org.osiam" })
+public class RootConfig {
+	
+	@Bean
+	public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
+		PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+		ppc.setLocation(new ClassPathResource("/persistence.properties"));
+		return ppc;
+	}
+	
+}
---
+++ authorization-server/src/main/java/org/osiam/signin/SigninController.java
@@ -0,0 +1,11 @@
+package org.osiam.signin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SigninController {
+
+	@RequestMapping(value = "signin")
+	public void signin() {}
+}
