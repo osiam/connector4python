5e4c99b3e3d758cf8fb68c73e75a8602c9fb6b49
Philipp Eder <p.eder@tarent.de>
2013-02-27 14:03:57 +0100

    stripped down the demo application
    stripped down the demo application for our basic needs, the next step will
    be to do the configuration via java in favour of xml

--- authorization-server/getAccessToken.sh
+++ authorization-server/getAccessToken.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-CODE=8cjsm6
+CODE=dhf7ga
 curl -X POST -d "client_id=tonr&client_secret=secret&code=$CODE&grant_type=authorization_code&redirect_uri=http://localhost:8080/tonr2/demo.html" http://localhost:8080/authorization-server/oauth/token
