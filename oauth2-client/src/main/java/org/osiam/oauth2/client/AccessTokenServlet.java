7d8d2650bc7167887b72361f6946d1c43cfc7841
Philipp Eder <p.eder@tarent.de>
2013-03-08 16:27:25 +0100

    removed unnecessary if

--- oauth2-client/src/main/java/org/osiam/oauth2/client/AccessTokenServlet.java
+++ oauth2-client/src/main/java/org/osiam/oauth2/client/AccessTokenServlet.java
@@ -46,15 +46,9 @@ public class AccessTokenServlet {
         String tokenUrl = environment + "/authorization-server/oauth/token";
         String combined = CLIENT_ID + ":" + CLIENT_SECRET;
         String redirectUri = req.getScheme() + "://" + req.getServerName() + ":8080" + "/oauth2-client/accessToken";
-        sendAuthCodeToAuthorizationServerWhenCodeIsSent(code, tokenUrl, combined, redirectUri, req);
+        sendAuthCode(code, tokenUrl, combined, redirectUri, req);
         addAttributesToHttpRequest(req, CLIENT_ID, CLIENT_SECRET);
         return "parameter";
-    }
-
-    private void sendAuthCodeToAuthorizationServerWhenCodeIsSent(String code, String tokenUrl, String combined, String redirectUri, HttpServletRequest req) throws IOException {
-        if (code != null) {
-            sendAuthCode(code, tokenUrl, combined, redirectUri, req);
-        }
     }
 
     private void sendAuthCode(String code, String tokenUrl, String combined, String redirectUri, HttpServletRequest req) throws IOException {
