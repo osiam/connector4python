8977596c5a0a295e7f3e178c7faa93497b50eeac
jtodea <jochen.todea@googlemail.com>
2013-03-08 16:05:34 +0100

    OSNG9 - increased rule compliance

--- oauth2-client/src/main/java/org/osiam/oauth2/client/AccessTokenServlet.java
+++ oauth2-client/src/main/java/org/osiam/oauth2/client/AccessTokenServlet.java
@@ -8,23 +8,14 @@ import org.apache.commons.httpclient.methods.PostMethod;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.context.support.SpringBeanAutowiringSupport;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.logging.Logger;
 
 @Controller
