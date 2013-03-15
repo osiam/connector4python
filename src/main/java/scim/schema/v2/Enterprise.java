b89ce814055bc9f775f953f7bdfb302f8878b1d6
jtodea <jochen.todea@googlemail.com>
2013-03-15 17:53:28 +0100

    OSNG-10 - added two new classes to scim schema

---
+++ src/main/java/scim/schema/v2/Enterprise.java
@@ -0,0 +1,63 @@
+package scim.schema.v2;
+
+/**
+ * Java class for extension enterprise.
+ */
+public class Enterprise {
+
+    protected String employeeNumber;
+    protected String costCenter;
+    protected String organization;
+    protected String division;
+    protected String department;
+    protected Manager manager;
+
+
+    public String getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(String employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public String getCostCenter() {
+        return costCenter;
+    }
+
+    public void setCostCenter(String costCenter) {
+        this.costCenter = costCenter;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+
+    public void setDivision(String division) {
+        this.division = division;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+}
---
+++ src/main/java/scim/schema/v2/Manager.java
@@ -0,0 +1,59 @@
+package scim.schema.v2;
+
+/**
+ * Java class for manager complex type.
+ */
+public class Manager {
+
+    protected String managerId;
+    protected String displayName;
+
+    /**
+     * Gets the value of the managerId property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getManagerId() {
+        return managerId;
+    }
+
+    /**
+     * Sets the value of the managerId property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setManagerId(String value) {
+        this.managerId = value;
+    }
+
+    /**
+     * Gets the value of the displayName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    /**
+     * Sets the value of the displayName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDisplayName(String value) {
+        this.displayName = value;
+    }
+
+}
