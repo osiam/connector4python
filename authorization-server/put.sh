a393f3a6779c738d1d6fbdbf6c62d65d45b22fe4
Philipp Eder <p.eder@tarent.de>
2013-03-26 11:59:50 +0100

    changed from set list to getList add

--- authorization-server/put.sh
+++ authorization-server/put.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl -i -H "Accept: application/json" -H "Content-type: application/json" -H "Authorization: Bearer $@" -X PUT localhost:8080/authorization-server/User/fnord -d '{"externalId":"fnord","userName":"Arthur Dent","password":""}' && 
+curl -i -H "Accept: application/json" -H "Content-type: application/json" -H "Authorization: Bearer $@" -X PUT localhost:8080/authorization-server/User/marissa -d '{"externalId":"marissa","userName":"Arthur Dent","password":""}' && 
 echo -e "\n"
