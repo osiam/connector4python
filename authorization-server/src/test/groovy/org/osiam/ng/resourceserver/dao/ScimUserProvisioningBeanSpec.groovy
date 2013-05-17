5272c44a20deae645d44c6873f058ddb33ef082e
Philipp Eder <ederphpp@gmail.com>
2013-05-17 16:46:25 +0200

    secured root with oauth2

--- authorization-server/src/test/groovy/org/osiam/ng/resourceserver/dao/ScimUserProvisioningBeanSpec.groovy
+++ authorization-server/src/test/groovy/org/osiam/ng/resourceserver/dao/ScimUserProvisioningBeanSpec.groovy
@@ -19,8 +19,7 @@ class ScimUserProvisioningBeanSpec extends Specification {
     def userDao = Mock(UserDAO)
     def userEntity = Mock(UserEntity)
     def scimUser = Mock(User)
-    SCIMUserProvisioningBean scimUserProvisioningBean =
-        new SCIMUserProvisioningBean(userDao: userDao)
+    SCIMUserProvisioningBean scimUserProvisioningBean = new SCIMUserProvisioningBean(userDao: userDao)
 
 
     def "should be possible to get an user by his id"() {
