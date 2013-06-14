56b6b9314078d6424384d9f6c0346fe59f1ae027
jtodea <jochen.todea@googlemail.com>
2013-06-14 16:59:07 +0200

    OSNG-91 - added script to run predefined test cases, added script to define test cases with a defined syntax which is also usable for non programmes

--- scripts/lps_test_case_definition.py
+++ scripts/lps_test_case_definition.py
@@ -1,2 +1,4 @@
+#This script is for defining test cases
+
 tests = [{'resource':'user', 'method':'create', 'serial':'1', 'parallel':'1'},
 {'resource':'user', 'method':'create', 'serial':'2', 'parallel':'2'}]
