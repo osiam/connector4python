d219c28dc284f35897af8d5862211bd8190e0e09
jtodea <jochen.todea@googlemail.com>
2013-06-20 15:21:31 +0200

    OSNG-93 - removed example.py, fixed typo in sequence_10k.py

--- performance-tests/sequence_10k.py
+++ performance-tests/sequence_10k.py
@@ -2,9 +2,8 @@ name = 'sequence_10k'
 description = 'The results of this test will show how long the system needed to reach the abort criteria.'
 configuration = {'create': {'User': 10000, 'Group': 1000}}
 
-tests = [{'resource': 'User', 'method': 'create'},
-         {'resource': 'User', 'method': 'patch'},
-         {'resource': 'User', 'method': 'search'},
-         {'resource': 'User', 'method': 'put'},
-         {'resource': 'User', 'method': 'search'},
-         {'resource': 'User', 'method': 'delete'}]
+tests = [{'resource': 'User', 'method': 'search'},
+         {'resource': 'User', 'method': 'update'},
+         {'resource': 'User', 'method': 'replace'},
+         {'resource': 'User', 'method': 'delete'},
+         {'resource': 'Group', 'method': 'search'}]
